58c1e4d0f3c59a958e0b4e7d8d40ea0d55660af0
Mohamad JOUNI <mhmd.jouni@outlook.fr>
2022-07-22 18:27:46 +0200

    Add private methods in the AOADMM classes

--- src/ao_admm.py
+++ src/ao_admm.py
@@ -42,6 +42,14 @@ class AbstractCPDAOADMM(ABC):
         ...
 
     @abstractmethod
+    def __postprocessing_normalize(self):
+        ...
+
+    @abstractmethod
+    def __postprocessing_sort_columns(self):
+        ...
+
+    @abstractmethod
     def __call__(self):
         ...
 
@@ -104,6 +112,14 @@ class AOADMMASC(AbstractCPDAOADMM):
             (self.tensor, slab[..., np.newaxis]), axis=-1
         )
 
+        # Get the tensor unfoldings, factor matrices, and dual variables
+        tensor_unfoldings = [
+            tl.unfold(self.tensor, mode).T for mode in range(self.tensor_order)
+        ]
+        dual_vars = [np.zeros_like(fm) for fm in self.factors0]
+
+        return self.factors0, tensor_unfoldings, dual_vars
+
     def __restore(self):
         """
         Restore the tensor and factor matrices to their original states.
@@ -111,7 +127,7 @@ class AOADMMASC(AbstractCPDAOADMM):
         self.tensor = self.tensor[..., :-1]
         self.factors[-1] = self.factors[-1][..., :-1]
 
-    def asc_update(
+    def __asc_update(
         self, delta: np.ndarray, beta: float = 1e-9,
     ):
         """
@@ -142,20 +158,8 @@ class AOADMMASC(AbstractCPDAOADMM):
         self.run(bsum=bsum)
 
     def run(self, bsum: float = 0):
-        # Initialize the factor matrices and the tensor
-        self.__initialize()
-
-        # Check if factor matrices are initialized
-        assert (
-            self.factors0 is not None
-        ), "The factor matrices must be initialized"
-
-        # Get the tensor unfoldings, factor matrices, and dual variables
-        tensor_unfoldings = [
-            tl.unfold(self.tensor, mode).T for mode in range(self.tensor_order)
-        ]
-        self.factors = self.factors0
-        dual_vars = [np.zeros_like(fm) for fm in self.factors0]
+        # Initialize
+        self.factors, tensor_unfoldings, dual_vars = self.__initialize()
 
         # Loop over the number of iterations
         for itr in range(self.n_iters):
@@ -194,16 +198,124 @@ class AOADMMASC(AbstractCPDAOADMM):
 
             # Here, it assumes that the data tensor and the factor matrices
             #   are corrected for the ASC constraint
-            tensor_unfoldings = self.asc_update(delta=self.tensor_mean,)
+            tensor_unfoldings = self.__asc_update(delta=self.tensor_mean,)
 
         # Restore the tensor and factor matrices to their original states
         self.__restore()
 
         # Normalize the factors into the diagonal entries
+        self.__postprocessing_normalize()
+
+        # Sort the factors by the diagonal entries in descending order
+        self.__postprocessing_sort_columns()
+
+
+@dataclass
+class AOADMMASCNaive(AbstractCPDAOADMM):
+    """
+    Solve CPD using AO (mainly through ADMM)
+    Future development may include:
+     - Loris-Verhoeven solver for the factor matrices
+    Current version supposes the following:
+     - A has nonnegativity, asc, and sparsity constraints
+     - B has nonnegativity, with normalized columns (l2 norm)
+     - C has nonnegativity, with normalized columns (l2 norm)
+     - The norms of B and C constitute the weights of the diagonal core tensor
+    """
+
+    def __post_init__(self):
+        super().__post_init__()
+
+    def __initialize(self):
+        """
+        Initialize the factor matrices for the ASC update.
+        The factor matrices are the absolute of the randn generation. Then:
+          The 1st factor gets the sum-to-one constraint.
+          The 2nd and 3rd factors are normalized column-wise.
+        :return:
+        """
+        # Initialize the factor matrices
+        self.factors0 = [
+            np.abs(np.random.randn(dim, self.tensor_rank))
+            for dim in self.tensor.shape
+        ]
+
+        # The 1st factor gets the sum-to-one constraint
+        self.factors0[-2] = normalize(self.factors0[-2], norm="l1", axis=1)
+
+        # The 2nd and 3rd factors are normalized column-wise
+        self.factors0[-1] = normalize(self.factors0[-1], norm="l2", axis=0)
+        self.factors0[-3] = normalize(self.factors0[-3], norm="l2", axis=0)
+
+        # Get the tensor unfoldings, factor matrices, and dual variables
+        tensor_unfoldings = [
+            tl.unfold(self.tensor, mode).T for mode in range(self.tensor_order)
+        ]
+        dual_vars = [np.zeros_like(fm) for fm in self.factors0]
+
+        return self.factors0, tensor_unfoldings, dual_vars
+
+    def __postprocessing_normalize(self):
+        # Normalize the factors into the diagonal entries
         self.diagonal = la.norm(self.factors[0], axis=0)
         self.factors[0] /= self.diagonal
 
+    def __postprocessing_sort_columns(self):
         # Sort the factors by the diagonal entries in descending order
         diagonal_indices = np.argsort(self.diagonal)[::-1]
         self.diagonal = self.diagonal[diagonal_indices]
         self.factors = [fm[:, diagonal_indices] for fm in self.factors]
+
+    def __asc_update(self):
+        self.factors[-2] = normalize(self.factors[-2], norm="l1", axis=1)
+
+    def __call__(self, bsum: float = 0):
+        self.run(bsum=bsum)
+
+    def run(self, bsum: float = 0):
+        # Initialize
+        self.factors, tensor_unfoldings, dual_vars = self.__initialize()
+
+        # Loop over the number of iterations
+        for itr in range(self.n_iters):
+            # Loop over the number of modes (i.e. factor matrices)
+            for mode in range(self.tensor_order):
+                # Solve the current sub-problem with ADMM
+                kr_product = tl.tenalg.khatri_rao(
+                    matrices=self.factors[:mode] + self.factors[mode + 1 :]
+                )
+
+                self.factors[mode], dual_vars[mode] = self.admms[mode](
+                    tensor_unfolding=tensor_unfoldings[mode],
+                    kr_product=kr_product,
+                    factor=self.factors[mode],
+                    dual_var=dual_vars[mode],
+                    bsum=bsum,
+                )
+
+                # Update A with the ASC constraint
+                self.__asc_update()
+
+            # Normalize the columns except for factors of indexes 0 and -2
+            # Absorb the weights into the factor matrix of index 0
+            weights = la.norm(self.factors[-1], axis=0)
+            self.factors[-1] /= weights
+            self.factors[0] *= weights
+
+            # Reconstruct the tensor from the estimated factor matrices
+            recons_tensor = tl.cp_tensor.cp_to_tensor((None, self.factors))
+
+            # Compare the reconstruction with the original tensor (RMSE)
+            self.recons_error[itr] = (
+                la.norm(recons_tensor - self.tensor)
+                / self.tensor_norm
+            )
+
+            # # Update the BSUM parameter if necessary
+            # bsum = 1e-7  + 0.01 * self.recons_error[itr]
+
+        # Normalize the factors into the diagonal entries
+        self.__postprocessing_normalize()
+
+        # Sort the factors by the diagonal entries in descending order
+        self.__postprocessing_sort_columns()
